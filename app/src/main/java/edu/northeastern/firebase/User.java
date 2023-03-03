252a586afaa14e1790e7067592e11ba02a94a89e
Chen Chen <chen.chencc@outlook.com>
2023-03-02 16:03:26 -0800

    Added User and Database Access Object (DAOUser)

---
+++ app/src/main/java/edu/northeastern/firebase/User.java
@@ -0,0 +1,20 @@
+package edu.northeastern.firebase;
+
+public class User {
+    private String userName;
+
+    public User(){};
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+}
