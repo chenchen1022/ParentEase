4290aec858f24251a3bc65f0015337f96eec9a4b
LinHanMSCS <linhanmscs@gmail.com>
2023-03-02 20:47:54 -0800

    Add MessagingService java file

---
+++ app/src/main/java/edu/northeastern/firebase/MessagingService.java
@@ -0,0 +1,12 @@
+package edu.northeastern.firebase;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MessagingService extends FirebaseMessagingService {
+
+
+
+    // TO DO
+
+
+}
